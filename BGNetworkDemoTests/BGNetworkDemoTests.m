6c2561c2e4339cc1c28bd89cd891babea790027a
chunguiLiu <404468494@qq.com>
2015-12-21 13:36:36 +0800

    add testThatRequestSuccess

--- BGNetworkDemoTests/BGNetworkDemoTests.m
+++ BGNetworkDemoTests/BGNetworkDemoTests.m
@@ -8,15 +8,19 @@
 
 #import <UIKit/UIKit.h>
 #import <XCTest/XCTest.h>
+#import "DemoNetworkConfiguration.h"
+#import "BGNetworkManager.h"
+#import "DemoRequest.h"
 
 @interface BGNetworkDemoTests : XCTestCase
-
 @end
 
 @implementation BGNetworkDemoTests
 
 - (void)setUp {
     [super setUp];
+    //set network configuration
+    [[BGNetworkManager sharedManager] setNetworkConfiguration:[DemoNetworkConfiguration configuration]];
     // Put setup code here. This method is called before the invocation of each test method in the class.
 }
 
@@ -35,6 +39,22 @@
     [self measureBlock:^{
         // Put the code you want to measure the time of here.
     }];
+}
+
+- (void)testThatRequestSuccess {
+    XCTestExpectation *expectation = [self expectationWithDescription:@"Request should succeed"];
+    __block BOOL requestSuccess = NO;
+    __block id blockResponseObject = nil;
+    DemoRequest *request = [[DemoRequest alloc] initPage:0 pageSize:10];
+    [request sendRequestWithSuccess:^(BGNetworkRequest * _Nonnull request, id  _Nullable response) {
+        requestSuccess = YES;
+        blockResponseObject = response;
+        [expectation fulfill];
+    } businessFailure:NULL networkFailure:NULL];
+    [self waitForExpectationsWithTimeout:10.0 handler:NULL];
+    
+    XCTAssertTrue(requestSuccess);
+    XCTAssertNotNil(blockResponseObject);
 }
 
 @end
